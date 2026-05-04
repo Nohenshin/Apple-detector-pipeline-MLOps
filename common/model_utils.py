ef56f1c095a298a807a1cb85f530742ceeb091c2
Ho Nhat Thanh <youareyou7.com@gmail.com>
2026-05-05 02:38:03 +0700

    pretrain = default

--- common/model_utils.py
+++ common/model_utils.py
@@ -4,6 +4,7 @@
 
 import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection import FasterRCNN_ResNet50_FPN_Weights
 
 def get_model(num_classes=3, pretrained=True):
     """
@@ -11,11 +12,8 @@ def get_model(num_classes=3, pretrained=True):
     num_classes: số lớp (bao gồm background). Ví dụ: 3 = background + Green Apple + Red Apple.
     pretrained: nếu True, dùng trọng số đã huấn luyện trên COCO.
     """
-    if pretrained:
-        model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-    else:
-        model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False)
-    # Thay đổi đầu phân loại
+    weights = FasterRCNN_ResNet50_FPN_Weights.DEFAULT if pretrained else None
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=weights)
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
     return model
